6bc406a198c9cda46bd4ab7d0db8bc0a24b86ab6
alessiabrg <152208379+alessiabrg@users.noreply.github.com>
2024-03-15 14:25:34 +0100

    Update Part_1_Q3.py

--- Part_1_Q3.py
+++ Part_1_Q3.py
@@ -1,1 +1,18 @@
+import numpy as np
+from scipy.sparse import csr_matrix
+from scipy.linalg import eigvals
+from scipy.linalg import svd
 
+
+class matrix: #create a class
+    def __init__(self, matrix1):
+        #store the matrix1 as an attribute of the class
+        self.matrix1 = np.array(matrix1)
+        
+    def eigenvalues(self):
+        values = eigvals(self.matrix1)
+        return values
+    
+    def decomposition(self):
+        U, S, Vt = svd(self.matrix1)
+        return U, S, Vt
