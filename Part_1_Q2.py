2465290d8562405c76fe3f7f1032656e0b869e32
Ulyssem23 <152270452+Ulyssem23@users.noreply.github.com>
2024-03-15 13:35:28 +0100

    Update Part_1_Q2.py

--- Part_1_Q2.py
+++ Part_1_Q2.py
@@ -13,35 +13,35 @@ class noLibraryMatrix: #create a new class
 #for _ in range(len(self.M1)): iterates over the number of rows in M1, for each row, it creates a list of zeros
         result = [[0 for _ in range(len(M2[0]))] for _ in range(len(self.M1))]
         
-      for i in range(len(self.M1)):
+        for i in range(len(self.M1)):
     #loop over the rows of M1
-    for j in range(len(self.M2[0])):
+            for j in range(len(self.M2[0])):
         #loop over the columns of M2
-        for k in range(len(self.M2)):
+                for k in range(len(self.M2)):
             #loop over the rows of the second matrix M2 + columns of the first matrix M1
             #compute dot product between the ith row of M1 and the kth column of M2
-            result[i][j] += self.M1[i][k] * self.M2[k][j]
+                    result[i][j] += self.M1[i][k] * self.M2[k][j]
            #update each element of the result matrix
-         return result
-
-def MV(self, vector): #vector matrix multiplication
-        #store the vector as an attribute of the class
-        self.vector = vector
-        #check if the vector is one-dimensional 
-        is_one_dimensional = all(isinstance(x, (int, float)) for x in vector)
-        #if the vector is not one-dimensional, raise a ValueError
-        if not is_one_dimensional:
-            raise ValueError("Vector's dimension is incorrect")
-        #initialize a result list with zeros
-        result = [0] * len(self.M1)
-    
-        #iterate over the rows of M1
-        for i in range(len(self.M1)):
-            #iterate over the elements of the vector
-            for k in range(len(vector)):
-                #multiply  elements of M1 and the vector and add to the result
-                result[i] += self.M1[i][k] * vector[k]
         return result
+
+    def MV(self, vector): #vector matrix multiplication
+            #store the vector as an attribute of the class
+            self.vector = vector
+            #check if the vector is one-dimensional 
+            is_one_dimensional = all(isinstance(x, (int, float)) for x in vector)
+            #if the vector is not one-dimensional, raise a ValueError
+            if not is_one_dimensional:
+                raise ValueError("Vector's dimension is incorrect")
+            #initialize a result list with zeros
+            result = [0] * len(self.M1)
+        
+            #iterate over the rows of M1
+            for i in range(len(self.M1)):
+                #iterate over the elements of the vector
+                for k in range(len(vector)):
+                    #multiply  elements of M1 and the vector and add to the result
+                    result[i] += self.M1[i][k] * vector[k]
+            return result
 
     def ADD(self, M3): #addition
         self.M3 = M3  #store  M3 as an attribute of the class
@@ -59,21 +59,21 @@ def MV(self, vector): #vector matrix multiplication
         
         return result 
 
-  def SUB(self, M3): #substraction
-    #store  M3 as an attribute of the class
-    self.M3 = M3
-    #initialize an empty list to store the result matrix
-    result = []
-    #iterate over the rows of M1
-    for i in range(len(self.M1)):
-        #initialize an empty list to store each row of the result matrix
-        row = []
-        #iterate over the columns of M1
-        for j in range(len(self.M1[0])):
-            #subtract elements of M1 and M3 and append to the row
-            row.append(self.M1[i][j] - M3[i][j])
-        result.append(row)
-     return result
+    def SUB(self, M3): #substraction
+        #store  M3 as an attribute of the class
+        self.M3 = M3
+        #initialize an empty list to store the result matrix
+        result = []
+        #iterate over the rows of M1
+        for i in range(len(self.M1)):
+            #initialize an empty list to store each row of the result matrix
+            row = []
+            #iterate over the columns of M1
+            for j in range(len(self.M1[0])):
+                #subtract elements of M1 and M3 and append to the row
+                row.append(self.M1[i][j] - M3[i][j])
+            result.append(row)
+        return result
 
     def normL1(self):
         #The L1 norm of a matrix is defined as the maximum absolute column sum of the matrix.
@@ -90,9 +90,14 @@ def MV(self, vector): #vector matrix multiplication
                 col_sum += abs(self.M1[i][j])
             # Update the maximum absolute column sum if necessary
             max_col_sum = max(max_col_sum, col_sum)
-
         return max_col_sum
+    
+    def normL2(self):
+        #
 
+class DenseMatrix(noLibraryMatrix):
+    def __init__(self, M1):
+        noLibraryMatrix.__init__(M1)
 
 class SparseMatrix(noLibraryMatrix):
     def __init__(self, M1):
@@ -112,6 +117,4 @@ class SparseMatrix(noLibraryMatrix):
                 if element != 0:
                     row_result.append((i, j, element))  # Store (row, column, value)
             result.append(row_result)
-        
         return result
-
