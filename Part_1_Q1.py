ed76d02de81fa98db7600f74d41a7fc66e5de9e1
alessiabrg <152208379+alessiabrg@users.noreply.github.com>
2024-03-15 13:46:45 +0100

    Update Part_1_Q1.py

--- Part_1_Q1.py
+++ Part_1_Q1.py
@@ -4,29 +4,20 @@ from scipy.sparse import csr_matrix
 
 class matrix: #create a class
     def __init__(self, matrix1):
-        #store the matrix1 as an attribute of the class
-        self.matrix1 = np.array(matrix1)
+        self.matrix1 = np.array(matrix1) #store the matrix1 as an attribute of the class
 
 #create methods for matrix operations  
 
     def MM(self, matrix2):
-        #convert the matrix2 to a NumPy array
-        self.matrix2 = np.array(matrix2)
-        #perform matrix multiplication using np.dot
-        multiplication = np.dot(self.matrix1, self.matrix2)
-        #return the result of the multiplication
+        self.matrix2 = np.array(matrix2) #convert the matrix2 to a NumPy array
+        multiplication = np.dot(self.matrix1, self.matrix2)  #matrix multiplication using np.dot
         return multiplication
         
     def MV(self, vector):
-        #convert the vector to a NumPy array
-        vector = np.array(vector)
-        #check if the vector is a 1D array and has the correct dimension, vector.ndim != 1:checks if the vector's dimension is not equal to 1, vector.shape[0] != self.matrix1.shape[1]:checks if the length of the vector is not equal to the number of columns
-        if vector.ndim != 1 or vector.shape[0] != self.matrix1.shape[1]:
-            #raise an error if the vector's dimension is incorrect
-            raise ValueError("Vector's dimension is incorrect")
-        #perform matrix-vector multiplication using np.dot
-        multiplication = np.dot(self.matrix1, vector)
-        #return the result of the multiplication
+        vector = np.array(vector)  #convert the vector to a NumPy array
+        if vector.ndim != 1 or vector.shape[0] != self.matrix1.shape[1]:   #check if the vector is a 1D array and has the correct dimension, vector.ndim != 1:checks if the vector's dimension is not equal to 1, vector.shape[0] != self.matrix1.shape[1]:checks if the length of the vector is not equal to the number of columns
+            raise ValueError("Vector's dimension is incorrect")  #raise an error if the vector's dimension is incorrect
+        multiplication = np.dot(self.matrix1, vector)   #matrix-vector multiplication using np.dot
         return multiplication
       
     def ADD(self, matrix3):
