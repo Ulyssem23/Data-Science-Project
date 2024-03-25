b4da2a17afef89cc6943172b2324cc11dd486e37
alessiabrg <152208379+alessiabrg@users.noreply.github.com>
2024-03-25 15:56:59 +0100

    NEWPART1.py

---
+++ NEWPART1.py
@@ -0,0 +1,156 @@
+#MAIN
+import Part_1
+import Part_1_Q_5
+import numpy as np
+import random
+from scipy.sparse import random
+import sys
+
+Random_100x100_matrix = np.random.randint(0, 101, size=(100, 100))
+Random_100x100_matrix2 = np.random.randint(0, 101, size=(100, 100))
+Random_200x200_matrix = np.random.randint(0, 101, size=(200, 200))
+Random_200x200_matrix2 = np.random.randint(0, 101, size=(200, 200))
+Random_300x300_matrix = np.random.randint(0, 101, size=(300, 300))
+Random_300x300_matrix2 = np.random.randint(0, 101, size=(300, 300))
+Random_400x400_matrix = np.random.randint(0, 101, size=(400, 400))
+Random_400x400_matrix2 = np.random.randint(0, 101, size=(400, 400))
+Random_500x500_matrix = np.random.randint(0, 101, size=(500, 500))
+Random_500x500_matrix2 = np.random.randint(0, 101, size=(500, 500))
+
+Random_100x100_Sparse_matrix = random(100, 100, density=0.01, format='csr')
+Random_200x200_Sparse_matrix = random(200, 200, density=0.01, format='csr')
+Random_300x300_Sparse_matrix = random(300, 300, density=0.01, format='csr')
+Random_400x400_Sparse_matrix = random(400, 400, density=0.01, format='csr')
+Random_500x500_Sparse_matrix = random(500, 500, density=0.01, format='csr')
+
+Random_100x1_vector = np.random.randint(0, 101, size=(100, ))
+Random_200x1_vector = np.random.randint(0, 101, size=(200, ))
+Random_300x1_vector = np.random.randint(0, 101, size=(300, ))
+Random_400x1_vector = np.random.randint(0, 101, size=(400, ))
+Random_500x1_vector = np.random.randint(0, 101, size=(500, ))
+
+Random_100x0_vector = [np.random.randint(0, 100) for _ in range(100)]
+Random_200x0_vector = [np.random.randint(0, 100) for _ in range(200)]
+Random_300x0_vector = [np.random.randint(0, 100) for _ in range(300)]
+Random_400x0_vector = [np.random.randint(0, 100) for _ in range(400)]
+Random_500x0_vector = [np.random.randint(0, 100) for _ in range(500)]
+
+choice = int(sys.argv[1])
+
+matrixlibrary = Part_1.matrix_Q1(Random_500x500_matrix)
+
+if choice == 1:
+    print(matrixlibrary.MM(Random_500x500_matrix2))
+
+if choice == 2:
+    print(matrixlibrary.MV(Random_500x1_vector))
+    
+if choice == 3:
+    print(matrixlibrary.ADD(Random_500x500_matrix2))
+    
+if choice == 4:
+    print(matrixlibrary.SUB(Random_500x500_matrix2))
+denselibrary = Part_1.DenseMatrix((Random_500x500_matrix))
+sparselibrary = Part_1.SparseMatrix((Random_500x500_matrix))
+
+matrixNOlibrary = Part_1.noLibraryMatrix((Random_500x500_matrix))
+
+if choice == 5:
+    print(matrixNOlibrary.MM(Random_500x500_matrix2))
+    
+if choice == 6:
+    print(matrixNOlibrary.MV(Random_500x0_vector))
+    
+if choice == 7:
+    print(matrixNOlibrary.ADD(Random_500x500_matrix2))
+    
+if choice == 8:
+    print(matrixNOlibrary.SUB(Random_500x500_matrix2))
+    
+if choice == 9:
+    print(matrixNOlibrary.normL1())
+    
+if choice == 10:
+    print(matrixNOlibrary.normL2())
+    
+if choice == 11:
+    print(matrixNOlibrary.normLinf())
+
+if choice == 12:
+    print(matrixNOlibrary.transpose())
+    
+if choice == 13:
+    print(matrixNOlibrary.power_iteration())
+    
+if choice == 14:
+    print(matrixNOlibrary.compute_svd())
+
+denseNOlibrary = Part_1.DenseMatrix(Random_500x500_matrix2)
+
+if choice == 15:
+    print(denseNOlibrary.gaussian_elimination(Random_500x1_vector))
+
+
+sparseNOlibrary = Part_1.SparseMatrix(Random_500x500_matrix2)
+
+if choice == 16:
+    print(sparseNOlibrary.sparseMM(((0, 2), (0, 5), (1, 0))))
+
+if choice == 17:
+    print(sparseNOlibrary.jacobi_method(Random_500x0_vector, Random_500x500_matrix2))
+
+matrixQ3 = Part_1.matrix_Q3(((1, 0), (0,1)))
+
+if choice == 18:
+    print(matrixQ3.eigenvalues())
+
+if choice == 19:
+    print(matrixQ3.decomposition())
+
+if choice == 20:
+    Part_1_Q_5.MM_Q1(10)
+    
+if choice == 21:
+    Part_1_Q_5.MV_Q1(10)
+    
+if choice == 22:
+    Part_1_Q_5.SUB_Q1(10)
+    
+if choice == 23:
+    Part_1_Q_5.MM_Q2(2)
+    
+if choice == 24:
+    Part_1_Q_5.MV_Q2(10)
+    
+if choice == 25:
+    Part_1_Q_5.ADD_Q2(10)
+    
+if choice == 26:
+    Part_1_Q_5.SUB_Q2(10)
+    
+if choice == 27:
+    Part_1_Q_5.NORML1_Q2(10)
+    
+if choice == 28:
+    Part_1_Q_5.NORML2_Q2(10)
+    
+if choice == 29:
+    Part_1_Q_5.NORMLINF_Q2(10)
+    
+if choice == 30:
+    Part_1_Q_5.TRANSPOSE_Q2(10)
+    
+if choice == 31:
+    Part_1_Q_5.power_iteration_Q2(2)
+    
+if choice == 32:
+    Part_1_Q_5.compute_svd_Q2(2)
+    
+if choice == 33:
+    Part_1_Q_5.gaussian_elimination_Q3(5)
+    
+if choice == 34:
+    Part_1_Q_5.sparse_MM_Q3(2)
+
+if choice == 35:
+    Part_1_Q_5.jacobi_method_Q3(10)
